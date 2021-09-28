e60edaa19f7d59f3c0c6dc19901501e84a4d8228
FrancescoLenolli <francescopietrolenolli@gmail.com>
2021-09-29 00:11:19 +0200

    Simple parallax effect

---
+++ Beat 'em Up/Assets/Scripts/ParallaxMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float effectMultiplier = .5f;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    private void Awake()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * effectMultiplier, 0, 0);
+        lastCameraPosition = cameraTransform.position;
+    }
+}
